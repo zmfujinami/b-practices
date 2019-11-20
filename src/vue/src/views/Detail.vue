7f748225d31cc0c1b6c04969c4b4da5c1130f508
zmfujinami <masaki-fujinami@zanthing.com>
2019-11-20 14:13:42 +0900

    追加

---
+++ src/vue/src/views/Detail.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    detail{{post_id}}
+  </div>
+</template>
+
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+
+  @Component({})
+  export default class Detail extends Vue {
+    post_id: number = 0
+    created() {
+      this.post_id = Number(this.$route.params['id'])
+    }
+  }
+</script>

f9f58f6fe65af7c21994a6359e7f7bac2924905e
zmfujinami <masaki-fujinami@zanthing.com>
2019-11-20 14:13:26 +0900

    vue ts化初期

--- src/vue/src/views/Home.vue
+++ src/vue/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="home">
+        <img alt="Vue logo" src="../assets/logo.png">
+    </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+  import {Component, Vue} from 'vue-property-decorator';
+  // @ is an alias to /src
+  import HelloWorld from '@/components/HelloWorld.vue'
 
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
+  @Component({})
+  export default class Home extends Vue {
+
   }
-}
 </script>
